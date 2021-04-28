20e39737e4dd49c5af8bdb8079a8af9f3ce746e9
thomson92 <t.kopaczyk@gmail.com>
2021-04-28 10:22:15 +0200

    pool manager fixes

--- UnityUtilities/PoolManager/Pooler.cs
+++ UnityUtilities/PoolManager/Pooler.cs
@@ -5,7 +5,7 @@ namespace UnityUtilities.PoolManager {
 
     public class ObjectPooler : MonoBehaviour, IBasePool {
 
-        protected BasePoolable ObjectPrefab;
+        private static Dictionary<string, BasePoolable> _prefabCache;
         private static Dictionary<string, Stack<BasePoolable>> _gameObjectPool;
         private static ObjectPooler _instance;
 
@@ -15,6 +15,7 @@ namespace UnityUtilities.PoolManager {
                     var gameObject = new GameObject();
                     _instance = gameObject.AddComponent<ObjectPooler>();
                     _gameObjectPool = new Dictionary<string, Stack<BasePoolable>>();
+                    _prefabCache = new Dictionary<string, BasePoolable>();
                 }
 
                 return _instance;
@@ -25,45 +26,64 @@ namespace UnityUtilities.PoolManager {
             if(!_gameObjectPool.TryGetValue(objectKey, out var indicatedPool))
                 Debug.LogError("Object pool not found for given key.");
 
-            var value = indicatedPool.Count > 0 ? indicatedPool.Pop() : InstantiateObject();
+            var value = indicatedPool.Count > 0 ? indicatedPool.Pop() : InstantiateObject(objectKey);
 
             return value;
         }
 
-        public void PopulatePool(int amount, string pathToObjectPrefab) {
-            if(ObjectPrefab == null)
-                ObjectPrefab = TryLoadGameObject(pathToObjectPrefab);
+        /// <summary>
+        /// Add unity game object to pool.
+        /// </summary>
+        /// <param name="amount">Amount of game object clones to add to pool.</param>
+        /// <param name="pathToObjectPrefab">Path in Resource folder where prefab is located eg. "FloatingEvents/FloatingText".</param>
+        /// <returns>Pooled object key. It's needed to fetch pooled game object from pool.</returns>
+        public string PopulatePool(int amount, string pathToObjectPrefab) {
+            var prefab = TryLoadGameObject(pathToObjectPrefab);
 
-            var key = ObjectPrefab.GetPoolKey();
+            var key = prefab.GetPoolKey();
+
+            if(!_prefabCache.ContainsKey(key))
+                _prefabCache.Add(key, prefab);
 
             if(!_gameObjectPool.TryGetValue(key, out var indicatedPool))
                 indicatedPool = new Stack<BasePoolable>();
 
             for(var i = 0; i < amount; i++) {
-                var el = InstantiateObject();
+                var el = InstantiateObject(prefab);
                 indicatedPool.Push(el);
             }
 
             _gameObjectPool.Add(key, indicatedPool);
+
+            return key;
         }
 
-        public void PopulatePool(int amount, BasePoolable prefab) {
-            ObjectPrefab = prefab;
-
+        /// <summary>
+        /// Add unity game object to pool.
+        /// </summary>
+        /// <param name="amount">Amount of game object clones to add to pool.</param>
+        /// <param name="prefab">Prefab refernce which will be added to pool.</param>
+        /// <returns>Pooled object key. It's needed to fetch pooled game object from pool.</returns>
+        public string PopulatePool(int amount, BasePoolable prefab) {
             if(prefab == null)
                 Debug.LogError("No prefab passed");
 
-            var key = ObjectPrefab.GetPoolKey();
+            var key = prefab.GetPoolKey();
+
+            if(!_prefabCache.ContainsKey(key))
+                _prefabCache.Add(key, prefab);
 
             if(!_gameObjectPool.TryGetValue(key, out var indicatedPool))
                 indicatedPool = new Stack<BasePoolable>();
 
             for(var i = 0; i < amount; i++) {
-                var el = InstantiateObject();
+                var el = InstantiateObject(prefab);
                 indicatedPool.Push(el);
             }
 
             _gameObjectPool.Add(key, indicatedPool);
+
+            return key;
         }
 
         public void ReturnObjectToPool(string objectKey, BasePoolable obj) {
@@ -74,19 +94,52 @@ namespace UnityUtilities.PoolManager {
             indicatedPool.Push(obj);
         }
 
-        protected virtual BasePoolable InstantiateObject() {
-            var obj = Instantiate(ObjectPrefab);
+        protected virtual BasePoolable InstantiateObject(BasePoolable prefab) {
+            var obj = Instantiate(prefab);
+
+            if(obj == null)
+                Debug.LogError($"Can not instantiate {prefab.name} for pool.");
 
             // Get reference of poolable component
-            var component = obj.GetComponent<BasePoolable>();
+            //var component = obj.GetComponent<BasePoolable>();
 
-            if(component == null) {
-                Debug.LogWarning("No poolable component attached to the object");
-            } else {
-                component.OnAddToPool();
+            //if(component == null) {
+            //    Debug.LogWarning("No poolable component attached to the object");
+            //} else {
+            //    component.OnAddToPool();
+            //}
+            obj.OnAddToPool();
+
+            return obj;
+        }
+
+        protected virtual BasePoolable InstantiateObject(string key)
+        {
+            BasePoolable obj;
+
+            if(_prefabCache.TryGetValue(key, out var cachedPrefab))
+            {
+                obj = Instantiate(cachedPrefab);
+            } else
+            {
+                obj = null;
+                Debug.LogError("Object prefab not found in cache");
             }
 
-            return component;
+            //var component = obj.GetComponent<BasePoolable>();
+
+            //if(component == null)
+            //{
+            //    Debug.LogWarning("No poolable component attached to the object");
+            //}
+            //else
+            //{
+            //    component.OnAddToPool();
+            //}
+
+            obj.OnAddToPool();
+
+            return obj;
         }
 
         private static BasePoolable TryLoadGameObject(string path) {
